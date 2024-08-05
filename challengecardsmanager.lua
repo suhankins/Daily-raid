e5be5a2f856c3bf51de7a31b3541493016cbd87c
suhankins <punishedbernadetta@niuitmo.ru>
2024-08-05 23:02:38 +0400

    attempt to index local 'remove_suggested_card' fix by BangL

--- challengecardsmanager.lua
+++ challengecardsmanager.lua
@@ -8,3 +8,12 @@ end)
 function ChallengeCardsManager:set_active_card_status_normal()
     self._active_card.status = ChallengeCardsManager.CARD_STATUS_NORMAL
 end
+
+-- By BangL <3
+-- fixes "attempt to index local 'remove_suggested_card' (a nil value)" crash
+local sync_remove_suggested_card_from_peer_original = ChallengeCardsManager.sync_remove_suggested_card_from_peer
+function ChallengeCardsManager:sync_remove_suggested_card_from_peer(peer_id, ...)
+    if self._suggested_cards and self._suggested_cards[peer_id] then
+        sync_remove_suggested_card_from_peer_original(self, peer_id, ...)
+    end
+end
