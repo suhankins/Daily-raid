6a425a762acd07d903f3dc1457302e7cacdb645a
suhankins <punishedbernadetta@niuitmo.ru>
2024-08-18 20:32:37 +0400

    removed gold awarding hack from the code

--- raidjobmanager.lua
+++ raidjobmanager.lua
@@ -3,24 +3,13 @@ Hooks:PreHook(RaidJobManager, "external_end_mission", "daily_raid_reward_gold", 
 	if self._current_job and Network:is_server() then
 		if not is_failed and DailyRaidManager.forced_card then
 			if managers.challenge_cards:get_active_card_status() ~= managers.challenge_cards.CARD_STATUS_FAILED then
-				-- This method isn't in u22, but will be added in the future
-				if managers.greed.secure_bounty then
-					managers.greed:secure_bounty(DailyRaidManager.daily_reward)
-				else
-					-- Hack-y way of awarding gold by telling clients we secured a lot of paintings
-					-- Adjusted for difficulty loot bonus
-					local paintings_required = math.ceil(DailyRaidManager.daily_reward /
-						managers.greed:greed_value_difficulty_multiplier())
-
-					for _ = 1, paintings_required, 1 do
-						-- Painting costs 1000g, so one gold bar
-						-- Second and third arguments are level_multiplier and "silent", both are unused
-						managers.loot:server_secure_loot('painting_sto', 1, false)
-					end
-				end
+				-- Awarding the bounty
+				managers.greed:secure_bounty(DailyRaidManager.daily_reward)
+				-- Notifying everyone in the lobby
 				DailyRaidManager:send_message("chat_message_daily_finished", {
 					GOLD_BARS = DailyRaidManager.daily_reward
 				})
+				-- Resetting everything and saving completion
 				DailyRaidManager:job_finished(DailyRaidManager.daily_seed)
 			end
 		end
